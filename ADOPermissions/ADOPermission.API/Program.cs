15e2daba302a5eabf2604d382d9a7adc56a657ed
Adam Jagocki <ajagocki@hotmail.com>
2021-03-22 14:40:34 +0100

    Add Serilog configured for elastic

--- ADOPermissions/ADOPermission.API/Program.cs
+++ ADOPermissions/ADOPermission.API/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using App.Metrics.AspNetCore;
 using App.Metrics.Formatters.Prometheus;
@@ -10,6 +11,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Elasticsearch;
 
 namespace ADOPermission.API
 {
@@ -18,6 +22,12 @@ namespace ADOPermission.API
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
+            Log.Logger = new LoggerConfiguration()
+                            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                            .Enrich.FromLogContext()
+                            .WriteTo.Console()
+                            .CreateBootstrapLogger(); 
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -32,19 +42,36 @@ namespace ADOPermission.API
                          endpointOptions.EnvironmentInfoEndpointEnabled = false;
                      };
                  })
+                .UseSerilog((context, services, configuration) => configuration
+                    .ReadFrom.Configuration(context.Configuration)
+                    .ReadFrom.Services(services)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Debug()
+                    .WriteTo.Console()
+                    .WriteTo.Elasticsearch(ConfigureElasticSink(context.Configuration, Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")))
+                    .Enrich.WithProperty("Environment", Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")))
                  .ConfigureServices((context, services) =>
                  {
-                     services.Configure<KestrelServerOptions>(
-                         context.Configuration.GetSection("Kestrel"));
+                     services.Configure<KestrelServerOptions>(context.Configuration.GetSection("Kestrel"));
                  }).ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-                //.ConfigureLogging( logginng =>
-                //{
-                //    logginng.ClearProviders();
-                //    logginng.AddDebug();
-                //    logginng.AddConsole();
-                //}                );
+        //.ConfigureLogging( logginng =>
+        //{
+        //    logginng.ClearProviders();
+        //    logginng.AddDebug();
+        //    logginng.AddConsole();
+        //}                );
+
+
+        private static ElasticsearchSinkOptions ConfigureElasticSink(IConfiguration configuration, string environment)
+        {
+            return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+            {
+                AutoRegisterTemplate = true,
+                IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+            };
+        }
     }
 }
